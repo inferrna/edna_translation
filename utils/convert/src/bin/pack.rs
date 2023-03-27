549be4ab1793f58433b2324e914a05a658205fc3
inferno <inferrna@gmail.com>
2023-03-27 16:15:24 +0400

    Working translation of verbs to english. TODO: interface translation

--- utils/convert/src/bin/pack.rs
+++ utils/convert/src/bin/pack.rs
@@ -1,26 +1,31 @@
 use std::fs::{File, OpenOptions};
 use std::io::{BufRead, BufReader, BufWriter, Read, Write};
 use std::path::PathBuf;
-use iconv::ffi::iconv;
 use convert::{CHUNK_OFFSET, CHUNK_SZ, ENCODING, MAGIC_NUMBERS, num};
 
 
 fn main() {
+
+    let src_path =  std::env::args()
+        .skip(1)
+        .last()
+        .expect("Provide source directory with txt and dat files, please");
+
     let mut kol: usize = 0;
-    while PathBuf::from(format!("{}.dat", num(kol))).exists() {
+    while PathBuf::from(format!("{src_path}/{}.dat", num(kol))).exists() {
         kol += 1;
     }
 
     let mut buf = vec![0u8; kol * CHUNK_SZ];
     let mut twobvec = vec![0;2];
     for i in 0..kol {
-        let mut txt_dat = BufReader::new(File::open(format!("{}.dat", num(i))).unwrap());
+        let mut txt_dat = BufReader::new(File::open(format!("{src_path}/{}.dat", num(i))).unwrap());
         txt_dat.read_exact(&mut twobvec).unwrap();
         buf[i*CHUNK_SZ] = twobvec[0];
         buf[i*CHUNK_SZ+1] = twobvec[1];
         drop(txt_dat);
 
-        let mut txt_txt = BufReader::new(File::open(format!("{}.txt", num(i))).unwrap());
+        let mut txt_txt = BufReader::new(File::open(format!("{src_path}/{}.txt", num(i))).unwrap());
         for j in 0..8 {
             let mut line = String::new();
             txt_txt.read_line(&mut line).unwrap();
